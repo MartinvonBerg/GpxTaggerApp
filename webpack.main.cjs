dc6730d5fd2bdd47b1080383e120a550892d6c4c
MartinvonBerg <mail@mvb1.de>
2026-02-20 13:41:24 +0100

    Change config to debug main.mjs (again)

--- webpack.main.cjs
+++ webpack.main.cjs
@@ -4,12 +4,14 @@ const path = require('path');
 module.exports = {
   //mode: 'production',
   target: 'electron-main',
+  devtool: 'source-map',
   entry: {
     main: './main.mjs',
   },
   output: {
     clean: true,
     filename: 'main.bundle.cjs',
+    devtoolModuleFilenameTemplate: '[absolute-resource-path]',
     path: path.resolve(__dirname, 'build'),
   },
   // möglichst wenige externals – alles, was hier nicht steht, landet im Bundle
